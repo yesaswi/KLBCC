186edf6f28d0c2867251105b8e0a28de7becf95b
Sai Yesaswi Vadapalli <yesaswi@Yesaswis-MacBook-Pro.local>
2019-10-08 16:51:07 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, '/public')))
+
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
